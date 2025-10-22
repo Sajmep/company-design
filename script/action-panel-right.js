9e873f0752285ba502f3a4592d82821b0ccfb970
fazlielahi <fazlielahi03060155124@gmail.com>
2025-10-22 19:15:10 +0300

    add apps css file and design

---
+++ script/action-panel-right.js
@@ -0,0 +1,9 @@
+function togglePanel() {
+    const panel = document.getElementById('actionPanel');
+    panel.classList.toggle('open');
+  }
+  
+  function closePanel() {
+    const panel = document.getElementById('actionPanel');
+    panel.classList.remove('open');
+  }
